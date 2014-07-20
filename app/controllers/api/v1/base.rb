bed12c47e7aa4c42fb622f64e45a0168c1ecae2f
yutaro <y.tanaka.kcbbf@gmail.com>
2014-07-21 02:22:24 +0900

    Install swagger.

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -1,6 +1,11 @@
 module API
   module V1
     class Base < Grape::API
+
+      add_swagger_documentation base_path: "/api",
+                                api_version: 'v1',
+                                hide_documentation_path: true
+
       mout API::V1::Hussars
       mout API::V1::Wings
     end
